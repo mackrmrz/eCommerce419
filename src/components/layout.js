c6aec4fda5ef0e70d29219ad4351fe38463aab28
Mack  Ramirez <mackramirez@Macks-MacBook-Pro.local>
2020-04-19 00:38:08 -0700

    Secondary commit

---
+++ src/components/layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Hi there </h2>
+        {/* {...this.props.children} */}
+      </div>
+    )
+  }
+}
+export default Layout;
