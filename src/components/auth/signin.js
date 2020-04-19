89911017244cc2223452e94292b7a98c2cbda9b6
Mack  Ramirez <mackramirez@Macks-MacBook-Pro.local>
2020-04-19 04:10:10 -0700

    Built SignUp and In components with a routes

---
+++ src/components/auth/signin.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Signin extends Component {
+    render(){
+        return(
+            <div className="signin">
+                <h2>Hi Mom</h2>
+            </div>
+        )
+    }
+}
+export default Signin;
---
+++ src/components/auth/signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Signup extends Component {
+    render(){
+        return(
+            <div className="signin">
+                <h2>Hi dad</h2>
+            </div>
+        )
+    }
+}
+export default Signup;
