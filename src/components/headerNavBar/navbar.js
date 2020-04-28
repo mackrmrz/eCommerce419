5ebc26698551425adc6be09e39efa8271f768284
Mack  Ramirez <mackramirez@Macks-MacBook-Pro.local>
2020-04-27 23:47:07 -0700

    connected navs and header to state

--- src/components/headerNavBar/navbar.js
+++ src/components/headerNavBar/navbar.js
@@ -2,7 +2,28 @@ import React, { Component } from "react";
 
 class Navbar extends Component {
     render(){
-        return <div className="navbar"><h1>Hi There Two</h1></div>
+        return (
+            <div className="navbar">
+                {
+                    this.props.navbarLinks.map(link,index) => {
+                        return(
+                            <a className='navbar__link' key={index} onClick={() => console.log('switch tabs')}>
+                                {link.title}
+                            </a>
+                        )
+                    }
+                }
+            </div>
+        )
     }
 }
+
+function mapStateToProps(state) {
+    const {navbarLinks} = state.navbarLinks;
+    return {
+        navbarLinks
+    }
+}
+
+Navbar = connect(mapStateToProps)(Navbar);
 export default Navbar;
